a28106a31289d2cfdebe8677697c03d8aa85519c
Alexandre Leote <alexandreleoteweb@gmail.com>
2024-10-28 15:55:13 +0100

    Exercice 1 alert window completed

--- Exercice 1/js/main.js
+++ Exercice 1/js/main.js
@@ -3,8 +3,16 @@
 const carreBtn = document.querySelector('.carre'); // target the form in order to click on it
 
 carreBtn.addEventListener('click', carreDescription); // add the description when clicked
+carreStyle = getComputedStyle(carreBtn);
 
+/* description displayed in the alert */
 var classDescription = `Class : ${carreBtn.className}`;
+classDescription += `\n - Background color : `+ carreStyle.backgroundColor;
+classDescription += `\n - Color : ` + carreStyle.color;
+classDescription += `\n - Height : `+ carreStyle.height;
+classDescription += `\n - Width : `+ carreStyle.width;
+classDescription += `\n - Display : ` + carreStyle.display;
+classDescription += `\n - Font : `+ carreStyle.fontFamily +" ("+ carreStyle.fontSize + ")";
 
 /* FUNCTIONS */
 

a6437f9841ff3c89b3ce3899b14b1fc57a690493
Alexandre Leote <alexandreleoteweb@gmail.com>
2024-10-28 19:54:24 +0100

    Exercice 2 function to get the color of the selected square

--- Exercice 2/js/main.js
+++ Exercice 2/js/main.js
@@ -1,14 +1,17 @@
 /* CONST */
 
-const colorSquare = document.querySelector('.square');
+const colorSquares = document.querySelectorAll('.square');
 const identifySquare = document.querySelector("#result");
-
-
-colorSquare.addEventListener('click', getColor);
-
 
 /* FUNCTIONS */
 
-function getColor () {
-    
-}
+colorSquares.forEach(square => { // For each square we add an event listener on the square when clicked
+    square.addEventListener('click', () => {
+
+        const bgColor = window.getComputedStyle(square).backgroundColor; // We retrieve the color of the clicked square
+
+        identifySquare.style.backgroundColor = bgColor; // Which we then apply on the background of the bottom square
+        identifySquare.textContent = bgColor; // Then we display the rgb code by replacing the text content inside the square
+
+    });
+});

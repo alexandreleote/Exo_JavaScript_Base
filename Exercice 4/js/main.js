9acefc8a7c7e6ee0f2fb5c45c8be105ddc15de51
Alexandre Leote <alexandreleoteweb@gmail.com>
2024-10-29 14:45:35 +0100

    Exercice 4 Completed

--- Exercice 4/js/main.js
+++ Exercice 4/js/main.js
@@ -5,22 +5,58 @@ const backgroundSite = document.querySelector('body');
 
 /* FUNCTIONS */
 
-mediaTargeted.forEach(media => { // browse the different element 'media'
-    media.addEventListener('click', () => { // Event listener on 'click
-        media.classList.toggle('active');
+// We create a method which will give a zoom impression
+function mediaZoom(event) {
 
-        const textMedia = media.querySelector('.text'); // We target the 'text' from the 'media' when clicked
-        const bgColor = window.getComputedStyle(media).backgroundColor; // We retrieve the color of the clicked square
+    // We target the 'text' from the 'media' when clicked
+    const textMedia = event.currentTarget.querySelector('.text'); 
 
+    // We target all the others 'text'
+    const otherText = document.querySelectorAll('.text');
+    
+    // If the media is active then when we click we remove its class 
+    if (event.currentTarget.classList.contains('active')) {
 
-        if (media.classList.contains('active')) { // We chceck if the 'media' contains the 'active' class
-            textMedia.classList.add('display') // Then we add the 'display' class to the 'text'
-            backgroundSite.style.backgroundColor = bgColor; // Which we then apply on the background of the bottom square
-        } else {
-            textMedia.classList.remove('display') // Or we remove it otherwise
-            backgroundSite.style.backgroundColor = ""; // Which we then apply on the background of the bottom square
-        }
+        event.currentTarget.classList.remove('active');
 
-    });
+        // And set the bg back to normal
+        backgroundSite.style.backgroundColor = "";
+
+        // We remove the text displayed
+        textMedia.classList.remove('display')
+
+    } else {
+
+        // Browse the different elements 'media'
+        mediaTargeted.forEach(media => {
+
+            // To which we remove the class 'active'
+            media.classList.remove('active');
+
+        });
+
+        // Browse the other texts
+        otherText.forEach(text => {
+
+            // To which we remove the class 'display'
+            text.classList.remove('display');
+
+        });
+
+        // We add the class 'active' to show the details in the media square
+        event.currentTarget.classList.add('active');
+
+        // We retrieve the color of the clicked square
+        backgroundSite.style.backgroundColor = window.getComputedStyle(event.currentTarget).backgroundColor; 
+
+        // We display the text below the icon
+        textMedia.classList.add('display');
+    }
+}
+
+// We call the function for evey media we target
+mediaTargeted.forEach(media => {
+    
+    media.addEventListener('click', (mediaZoom)); 
+
 });
-

b317c8a80271cc027aad4b7a1583ce6b0f491daf
Alexandre Leote <alexandreleoteweb@gmail.com>
2024-10-29 16:49:19 +0100

    Exercice 5 display of the first object of the array

--- Exercice 5/js/main.js
+++ Exercice 5/js/main.js
@@ -1,3 +1,18 @@
 import quotes from './quotes.js';
 
-const textQuote = document.querySelector('.text');
+const quoteText = document.querySelector('.text');
+
+// Console.log details
+Object.entries(quotes).forEach(([key, value]) => console.log(value));
+
+
+// Display it inside one div
+Object.entries(quotes).forEach(([key, value]) => {
+    quoteText.textContent += value.title + " ";
+    quoteText.textContent += value.content + " ";
+    quoteText.textContent += value.author;
+    
+    for (i = 0 ; i <= quotes.id.length ; i++) {
+        
+    }
+})
